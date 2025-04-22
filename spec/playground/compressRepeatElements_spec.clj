e094f0139d798c5ad28ffaf4b8e7128d405b3478
Austin Cripe <austincripe@Austins-Mac-mini.local>
2025-04-22 17:43:49 -0400

    compress repeat changes-complete

--- spec/playground/compressRepeatElements_spec.clj
+++ spec/playground/compressRepeatElements_spec.clj
@@ -4,6 +4,11 @@
 
 (describe "Compress Repeat Elements"
   (context "testing file"
-    (it "1 = 1 "
-      (should= 1 (mytest 5))))
+    (it "[1 1 2] gives [1 2]"
+      (should= [1 2] (compress [1 1 2])))
+    (it "[1 2 3 2 1]"
+      (should=  [1 2 3 2 1]  (compress [1 2 3 2 1])))
+    (it "StringTest"
+      (should=  ["a" "b" "a"]  (compress ["a" "a" "b" "b" "a" "a"])))
+    )
   )
--- src/playground/compressRepeatElements.clj
+++ src/playground/compressRepeatElements.clj
@@ -1,4 +1,20 @@
 (ns playground.compressRepeatElements)
 
-(defn mytest [n]
-  n)
+;Write a function that removes consecutive duplicates from a list.
+;You're only removing consecutive duplicates — not all duplicates
+;(compress [1 1 2 3 3 3 2 2 1])
+;;; => [1 2 3 2 1]
+;
+;(compress ["a" "a" "b" "b" "a" "a"])
+;;; => ["a" "b" "a"]
+;
+;(compress [])
+;;; => []
+(defn compress [coll]
+  (loop [i 0
+         new-coll []]
+    (if (= i (dec (count coll)))
+      (conj new-coll (nth coll i))
+      (if (not= (nth coll i) (nth coll (inc i)))
+        (recur (inc i) (conj new-coll (nth coll i)))
+        (recur (inc i) new-coll)))))
